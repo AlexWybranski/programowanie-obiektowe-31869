7231e7aeea30949c276f02c4028b7d0d145d384a
Alex Wybranski <alexwybranski222@gmail.com>
2025-10-12 17:10:27 +0200

    zad 1

---
+++ Zadanie 1/Zadanie1/Program.cs
@@ -0,0 +1,16 @@
+﻿string passwd = "admin123";
+
+while (true)
+{
+    Console.WriteLine("Please enter your password: ");
+    string input = Console.ReadLine();
+
+    if (input == passwd)
+    {
+        break;
+    }
+    else
+    {
+        continue;
+    }
+}
