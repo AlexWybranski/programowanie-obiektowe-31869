0d7798e2b56d5b2c97d02dfba92fe40e8eae3342
AlexWybranski <alexwybranski222@gmail.com>
2025-10-16 16:44:55 +0200

    task 7 not done, task 8 only created

---
+++ Zadanie7/Program.cs
@@ -0,0 +1,40 @@
+﻿/*Utwórz tablicę Zwierze[] z obiektami różnych zwierząt i wywołaj DajGlos() w
+pętli foreach.*/
+
+public class Zwierze
+{
+    public virtual void DajGlos() => Console.WriteLine("Zwierzę wydaje dźwięk");
+}
+
+class Pies : Zwierze
+{
+    public override void DajGlos() => Console.WriteLine("Hau hau!");
+}
+
+class Kot : Zwierze
+{
+    public override void DajGlos() => Console.WriteLine("Miau!");
+}
+
+class Krowa : Zwierze
+{
+    public override void DajGlos() => Console.WriteLine("Muu!");
+}
+
+class Swinia : Zwierze
+{
+    public override void DajGlos() => Console.WriteLine("Chrum!");
+}
+
+Zwierze[] zwierzeta =
+{
+    new Pies(),
+    new Kot(),
+    new Krowa(),
+    new Swinia()
+};
+
+foreach (object zwierze in zwierzeta)
+{
+    DajGlos();
+}
