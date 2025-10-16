3a13d510d003525e67ad7455588aefd5130a65df
AlexWybranski <alexwybranski222@gmail.com>
2025-10-16 16:00:56 +0200

    done task 4

---
+++ Zadanie4/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Person
+{
+    public string name;
+    public int age;
+
+    public void IntroduceYourself()
+    {
+        Console.WriteLine($"Hi, my name is {name} and i'm {age} years old!");
+    }
+    
+    public Person(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Person p1 = new Person("Alex" , 23);
+        Person p2 = new Person("Bob", 28);
+        Person p3 = new Person("John", 24);
+        Person p4 = new Person("Jane", 30);
+        Person p5 = new Person("Cthulhu", 782);
+        
+        p1.IntroduceYourself();
+        p2.IntroduceYourself();
+        p3.IntroduceYourself();
+        p4.IntroduceYourself();
+        p5.IntroduceYourself();
+    }
+}
