1f13805f330b83e004e0cbc3f552f0c417219ae4
AlexWybranski <alexwybranski222@gmail.com>
2025-10-16 16:21:39 +0200

    done task 6

---
+++ Zadanie6/Program.cs
@@ -0,0 +1,16 @@
+﻿//Dodaj klasę Kot, która również dziedziczy po Zwierze i ma metodę Miaucz().
+
+class Zwierze
+{
+    public void Jedz() => Console.WriteLine("Zwierzę je");
+}
+
+class Pies : Zwierze
+{
+    public void Szczekaj() => Console.WriteLine("Hau hau!");
+}
+
+class Kot : Zwierze
+{
+    public void Miaucz() => Console.WriteLine("Miau miau!");
+}
