0fbca865aad80e142ba04ea55a5f3ff876df1c5b
AlexWybranski <alexwybranski222@gmail.com>
2025-10-16 15:30:27 +0200

    done task 3

---
+++ Zadanie3/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] cities =
+{
+    "London",
+    "Beijing",
+    "Paris",
+    "New York",
+    "Warsaw"
+};
+
+foreach (string city in cities)
+{
+    Console.WriteLine($"City: {city}");
+}
