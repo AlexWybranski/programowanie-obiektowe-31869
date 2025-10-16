1d8b18b9f6ed6d598637021ed56f0d1db235a80b
AlexWybranski <alexwybranski222@gmail.com>
2025-10-16 16:08:41 +0200

    done task 5(?)

---
+++ Zadanie5/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Dodaj metodę Wyplata(double kwota), która wypłaca pieniądze tylko jeśli saldo
+jest wystarczające.*/
+
+public class KontoBankowe
+{
+    private double saldo;
+    public double PobierzSaldo() { return saldo; }
+   
+    public void Wplata(double kwota) { saldo += kwota; }
+    public void Wyplata(double kwota)
+    {
+        if (kwota > saldo)
+        {
+            Console.WriteLine("Niewystarczające środki na koncie!");
+        }
+        else
+        {
+            saldo -= kwota;
+        }
+    }
+}
+
