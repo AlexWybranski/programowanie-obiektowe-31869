45a373c1c01d90e9a1cba6435780dc66117701ff
AlexWybranski <alexwybranski222@gmail.com>
2026-01-07 14:21:50 +0100

    AutoKomis GUI

---
+++ AutoKomis/AutoKomis/Database/Vehicle.cs
@@ -0,0 +1,14 @@
+﻿using AutoKomis.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutoKomis.Database;
+
+public class VehicleDb : DbContext
+{
+    public VehicleDb(DbContextOptions options) : base(options)
+    {
+        
+    }
+    
+    public DbSet<Car> Cars { get; set; }
+}
