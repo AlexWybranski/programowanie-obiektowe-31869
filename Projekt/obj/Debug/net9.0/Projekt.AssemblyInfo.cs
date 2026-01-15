36236ddbe5a1678fdf76cf360ecd69422d4ec2e6
AlexWybranski <alexwybranski222@gmail.com>
2026-01-15 15:28:11 +0100

    finish 1.0

--- Projekt/obj/Debug/net9.0/Projekt.AssemblyInfo.cs
+++ Projekt/obj/Debug/net9.0/Projekt.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projekt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3908754f3b1287b1bcd66edd3a486ec72359c28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26f48675d3f0da821cfef5be04725e4921a5d2ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projekt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projekt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
