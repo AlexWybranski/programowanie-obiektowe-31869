1caa88b262c64be88f0fc27f277db795d3000f88
AlexWybranski <alexwybranski222@gmail.com>
2026-01-26 15:00:17 +0100

    Added automatic 0 value for digital editions

--- Projekt/obj/Debug/net9.0/Projekt.AssemblyInfo.cs
+++ Projekt/obj/Debug/net9.0/Projekt.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projekt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+918cb83e4b4a9cbe3cbcf3de41414087f2c89cba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+889f5d450754e752f6cf4d05d87f2071aebc83bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projekt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projekt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
